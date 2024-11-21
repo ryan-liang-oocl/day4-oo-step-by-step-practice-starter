676ccf722d74f586dcd34acf8b197fe6162d54ed
Ryan Liang <ryan.liang@oocl.com>
2024-11-21 13:44:35 +0800

    feat: step 1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,35 @@
 package oo;
 
+import java.beans.PersistenceDelegate;
+import java.util.Objects;
+
 public class Person {
+    private Integer id;
+
+    private String name;
+
+    private int age;
+
+    public Person(Integer id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object otherPerson) {
+        if (this == otherPerson) return true;
+        if (otherPerson == null || getClass() != otherPerson.getClass()) return false;
+        Person person = (Person) otherPerson;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
